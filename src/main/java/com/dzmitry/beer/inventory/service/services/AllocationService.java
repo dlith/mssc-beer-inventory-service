7f7db1e6f307862494c2fba0819b49520a296873
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-11-11 12:59:28 +0100

    handle allocation requests

--- src/main/java/com/dzmitry/beer/inventory/service/services/AllocationService.java
+++
@@ -1,8 +0,0 @@
-package com.dzmitry.beer.inventory.service.services;
-
-import com.dzmitry.sfg.brewery.model.BeerOrderDto;
-
-public interface AllocationService {
-
-    Boolean allocateOrder(BeerOrderDto beerOrderDto);
-}
---
+++ src/main/java/com/dzmitry/sfg/beer/inventory/service/config/JmsConfig.java
@@ -0,0 +1,26 @@
+package com.dzmitry.sfg.beer.inventory.service.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+public class JmsConfig {
+
+    public static final String NEW_INVENTORY_QUEUE = "new-inventory";
+    public static final String ALLOCATE_ORDER_QUEUE = "allocate-order";
+    public static final String ALLOCATE_ORDER_RESPONSE_QUEUE = "allocate-order-response";
+
+
+    @Bean
+    public MessageConverter messageConverter(ObjectMapper objectMapper) {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setObjectMapper(objectMapper);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
